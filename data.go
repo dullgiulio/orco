04836554b1a9b8798f9ad1d01d4b2123dc6f834e
Giulio Iotti <dullgiulio@gmail.com>
2016-01-18 19:50:01 +0200

    Nil checks for completeness

--- data.go
+++ data.go
@@ -3,13 +3,21 @@ package main
 type rowDiff map[int]string
 
 func makeRowDiff() rowDiff {
-    return make(map[int]string)
+	return make(map[int]string)
+}
+
+func makeDiffForRow(r row) rowDiff {
+	d := makeRowDiff()
+	for i := 0; i < len(r); i++ {
+		d[i] = r[i]
+	}
+	return d
 }
 
 type row []string
 
 func makeRow(s ...string) row {
-    return row(s)
+	return row(s)
 }
 
 // diff computes the difference between r and o,
@@ -17,19 +25,23 @@ func makeRow(s ...string) row {
 // When all elements match returns nil.
 // Diff also returns nil if len(r) != len(o).
 func (r row) diff(o row) rowDiff {
-    if len(r) != len(o) {
-        return nil
-    }
-    diff := makeRowDiff()
-    for i := 0; i < len(r); i++ {
-        if r[i] != o[i] {
-            diff[i] = o[i]
-        }
-    }
-    if len(diff) == 0 {
-        return nil
-    }
-    return diff
+	if o == nil {
+		return makeDiffForRow(r)
+	}
+	if r == nil {
+		return makeDiffForRow(o)
+	}
+	if len(r) != len(o) {
+		return nil
+	}
+	diff := makeRowDiff()
+	for i := 0; i < len(r); i++ {
+		if r[i] != o[i] {
+			diff[i] = o[i]
+		}
+	}
+	if len(diff) == 0 {
+		return nil
+	}
+	return diff
 }
-
-
--- data_test.go
+++ data_test.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestRowDiff(t *testing.T) {
-    r0 := makeRow("a", "b", "c")
-    r1 := makeRow("a")
+	r0 := makeRow("a", "b", "c")
+	r1 := makeRow("a")
 
-    if r0.diff(r1) != nil {
-        t.Error("expected nil on uncomparable rows")
-    }
+	if r0.diff(r1) != nil {
+		t.Error("expected nil on uncomparable rows")
+	}
 
-    if r0.diff(r0) != nil {
-        t.Error("expected nil on comparison with itself")
-    }
+	if r0.diff(r0) != nil {
+		t.Error("expected nil on comparison with itself")
+	}
 
-    r2 := makeRow("a", "c", "b")
-    d0 := r0.diff(r2)
-    if d0 == nil {
-        t.Fatal("expected non-nil diff between different rows")
-    }
-    d1 := makeRowDiff()
-    d1[1] = "c"
-    d1[2] = "b"
-    for k, v := range d1 {
-        if d0[k] != v {
-            t.Errorf("expected value %s: %s has %s", k, v, d0[k])
-        }
-    }
+	r2 := makeRow("a", "c", "b")
+	d0 := r0.diff(r2)
+	if d0 == nil {
+		t.Fatal("expected non-nil diff between different rows")
+	}
+	d1 := makeRowDiff()
+	d1[1] = "c"
+	d1[2] = "b"
+	for k, v := range d1 {
+		if d0[k] != v {
+			t.Errorf("expected value %s: %s has %s", k, v, d0[k])
+		}
+	}
 }
